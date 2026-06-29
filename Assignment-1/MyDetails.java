073fc055fdcd76ff62025178f000c55eefe9ff68
Mayank Raj <mayankraj212000@gmail.com>
2026-06-29 17:38:38 +0530

    Update and rename p1.java to Assignment-1/MyDetails.java

--- Assignment-1/MyDetails.java
+++ Assignment-1/MyDetails.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class p1{
+public class MyDetails{
     public static void main(String[]args){
         System.out.println("NAME:-MAYANK RAJ");
         System.out.println("BRANCH:-CSE");
