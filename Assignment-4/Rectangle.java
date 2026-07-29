4ee7d803042bd70427977554aee5e69b926afa2b
Mayank Raj <mayankraj212000@gmail.com>
2026-07-29 19:33:39 +0530

    Add files via upload

---
+++ Assignment-4/Rectangle.java
@@ -0,0 +1,30 @@
+import java.util.*;
+class Area
+{
+    int length;
+    int breadth;
+    public void setDim(int length,int breadth)
+    {
+       this.length=length;
+       this.breadth=breadth;
+    }
+
+public int getArea()
+{
+    return length * breadth;
+
+}
+}
+public class Rectangle{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        Area s1=new Area();
+        System.out.println("Enter The Length:");
+        int a=sc.nextInt();
+        System.out.println("Enter The Breadth:");
+        int b=sc.nextInt();
+        s1.setDim(a,b);
+        System.out.println("Area="+s1.getArea());
+    }
+}
+
